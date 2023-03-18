683c46ff63e7e5e98a3144dc0165153a6456555a
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-18 07:17:08 -0700

    update

---
+++ Number Theory/4. Sieve.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e6 + 9;
+bool f[N];
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> primes;
+  f[1] = true;
+  for(int i = 2; i * i <= n; i++) {
+    if(!f[i]) {
+      for(int j = i + i; j <= n; j += i) {
+        f[j] = true;
+      }
+    }
+  }
+  for(int i = 2; i <= n; i++) {
+    if(!f[i]) primes.push_back(i);
+  }
+  cout << primes.size() << endl;
+  for(auto x: primes) {
+    cout << x << ' ';
+  }
+}
