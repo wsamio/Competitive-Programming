b44638b82b7901cef89be50aba9606465389987a
Iftu Bin Misbah <samio.cse@gmail.com>
2023-03-18 02:18:35 -0700

    update

---
+++ Divisors.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin >> n;
+  vector<int> divs;
+  for(int i = 1; i * i <= n; i++){
+    if(n % i == 0) {
+      divs.push_back(i);
+      if(i != n / i)
+        divs.push_back(n / i);
+    }
+  }
+  sort(divs.begin(), divs.end());
+  for(auto x: divs) {
+    cout << x << ' ';
+  }
+  cout << endl;
+}
