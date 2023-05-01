d5a6c08690e84ff902034367cf3d1c8ef2ba11a5
Iftu Bin Misbah <samio.cse@gmail.com>
2023-05-01 21:30:26 +0600

    update

--- Number Theory/8. Euclid.cpp
+++ Number Theory/8. Euclid.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int GCD(int a, int b) {
-  if(a < b) swap(a, b);
-  if(b == 0) return a;
-  return GCD(a % b, b);
+  a = a % b;
+  if(a == 0) return b;
+  return GCD(b, a);
 }
 
 int main() {
