b0826356cdf1f5fa03fa5612c55fd5ae15b2ff27
Iftu Bin Misbah <samio.cse@gmail.com>
2023-05-01 22:02:30 +0600

    update

--- Number Theory/8. Euclid.cpp
+++ Number Theory/8. Euclid.cpp
@@ -1,10 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+
+//first approach
+
+int GCD(int a, int b) {
+  if(a < b) swap(a, b);
+  if(b == 0) return a;
+  return GCD(a % b, b);
+}
+
+//second approach
+
 int GCD(int a, int b) {
   a = a % b;
   if(a == 0) return b;
   return GCD(b, a);
+}
+
+*/
+
+//third approach
+
+int GCD(int a, int b) {
+    if(b == 0) return a;
+    return GCD(b, a % b);
 }
 
 int main() {
